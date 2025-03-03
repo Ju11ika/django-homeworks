377e21c0c7117f808b8f54a4239d8b635f865125
Ju11ika <jullika@live.ru>
2025-03-03 20:48:19 +0300

    RestApi hometask. SmartHome.

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import SensorListCreateView, SensorDetailUpdateView, MeasurementCreateView
 
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', SensorListCreateView.as_view()),
+    path('sensors/<pk>/', SensorDetailUpdateView.as_view()),
+    path('measurements/', MeasurementCreateView.as_view()),
 ]
